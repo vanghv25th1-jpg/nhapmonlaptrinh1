5a53e06a2bff30b2003d5fe7e09bc836ca61fc64
Administrator <Administrator@PMT2018>
2025-12-04 14:17:36 +0700

    dat09

--- buoi1/buoi5/buoi5.cpp
+++ buoi1/buoi5/buoi5.cpp
@@ -2,48 +2,75 @@
 #include <string>
 using namespace std;
 
-void cau1() {
-	int a, b;
-	cout << "Nhap a,b:";
-	cin >> a >> b;
-	if (a == 0) {
-		if (b == 0)
-			cout << "Phuong trinh vo so nghiem :" << endl;
-		else
-			cout << "Phuong trinh vo nghiem:" << endl;
+//void cau1() {
+//	int a, b;
+//	cout << "Nhap a,b:";
+//	cin >> a >> b;
+//	if (a == 0) {
+//		if (b == 0)
+//			cout << "Phuong trinh vo so nghiem :" << endl;
+//		else
+//			cout << "Phuong trinh vo nghiem:" << endl;
+//	}
+//	else {
+//		cout << "Nghiem la : " << (-b * 1.0 / a) << endl;
+//	}
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//void cau4() {
+//	int thang;
+//	cout << "Nhap thang :";
+//	cin >> thang;
+//	if (thang <= 3) {
+//		cout << "Quy 1";
+//	}
+//	else if (thang <= 6) {
+//		cout << "Quy 2";
+//	}
+//	else if (thang <= 9) {
+//		cout << " quy3";
+//	}
+//	else {
+//		cout << "Quy 4";
+//
+//	}
+//}
+
+
+
+void cau5() {
+	int a, b, c;
+	cout << "Nhap 3 canh a,b,c:";
+	cin >> a >> b >> c;
+	if (a + b > c || b + c > a || a + c > b) {
+		if ( a==b||b+c>a||a+c>b){
+		cout << " Tam giac deu " << endl;
+	}
+	else if (a == b || b == c || c == a) {
+		cout << "Tam giac can " << endl;
+	}
+	else if (b * b == a * a + c * c || a * a == b * b + c * c || c * c == a * a + b * b) {
+		cout << "Tam giac vuong " << endl;
 	}
 	else {
-		cout << "Nghiem la : " << (-b * 1.0 / a) << endl;
+		cout << "Tam giac thuong" << endl;
 	}
 }
-
-
-
-
-
-
-
-
-void cau4() {
-	int thang;
-	cout << "Nhap thang :";
-	cin >> thang;
-	if (thang <= 3) {
-		cout << "Quy 1";
-	}
-	else if (thang <= 6) {
-		cout << "Quy 2";
-	}
-	else if (thang <= 9) {
-		cout << " quy3";
-	}
 	else {
-		cout << "Quy 4";
+		cout << " Khong phai la tam giac" << endl;
+		}
+	
 
-	}
 }
 int main() 
 {
-	cau4();
+	cau5();
 
-}
+} 
